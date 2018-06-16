45741722c84c5dd941d895ee21c606eb9472e212
NannKhaingZar <nannkhaingzathinn@gmail.com>
2018-06-16 17:39:26 +0630

    Addef

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
 	greeting = "Hello World"
 	return render_template("index.html", greeting = greeting)
-	
 if __name__== '__main__':
 	app.run()
